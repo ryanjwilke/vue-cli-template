abea6e89414f0b64009065e080d9bd15277257f2
Ryan Wilke <ryanwilke@gmail.com>
2019-11-05 16:23:22 +0100

    Updated the snapshot

--- tests/unit/components/HelloWorld.spec.js
+++ tests/unit/components/HelloWorld.spec.js
@@ -2,10 +2,15 @@ import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld', () => {
-  const wrapper = shallowMount(HelloWorld)
+  const wrapper = shallowMount(HelloWorld, {
+    propsData: {
+      msg: 'Hello World',
+    },
+  })
 
   it('is a Vue instance', () => {
     expect(wrapper.isVueInstance()).toBeTruthy()
+    expect(wrapper.name()).toBe('HelloWorld')
   })
 
   it('renders correctly', () => {
