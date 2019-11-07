74cf4502d522ba9a860b01adc6dfca4e553864da
Ryan Wilke <ryanwilke@gmail.com>
2019-11-07 09:16:55 +0100

    add jest puppeteer config

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,8 @@
+// See https://github.com/smooth-code/jest-puppeteer for more information about these options
+module.exports = {
+  server: {
+    command: 'yarn vue:start',
+    port: 8080,
+    launchTimeout: 100000,
+  },
+}
